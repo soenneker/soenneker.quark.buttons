6956cde0b0cc36fd906244d1ca2e2f9227d160c2
Jake Soenneker <jake@soenneker.com>
2025-09-12 08:02:50 -0500

    Color and size support..

--- test/Soenneker.Quark.Buttons.Tests/ButtonTests.cs
+++ test/Soenneker.Quark.Buttons.Tests/ButtonTests.cs
@@ -6,11 +6,8 @@ namespace Soenneker.Quark.Buttons.Tests;
 [Collection("Collection")]
 public sealed class ButtonTests : FixturedUnitTest
 {
-    private readonly Button _button;
-
     public ButtonTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _button = Resolve<Button>(true);
     }
 
     [Fact]
