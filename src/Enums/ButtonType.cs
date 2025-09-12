3cc3563c06003a3cde44d9a56c3a59a01a8be860
Jake Soenneker <jake@soenneker.com>
2025-09-12 07:31:41 -0500

    misc

--- src/Enums/ButtonType.cs
+++ src/Enums/ButtonType.cs
@@ -1,15 +1,18 @@
 using Intellenum;
 
-namespace Soenneker.Quark.Buttons;
+namespace Soenneker.Quark.Buttons.Enums;
 
 /// <summary>
 /// Button type enumeration.
 /// </summary>
 [Intellenum<string>]
-public partial class ButtonType
+public sealed partial class ButtonType
 {
     public static readonly ButtonType Button = new("button");
+
     public static readonly ButtonType Submit = new("submit");
+
     public static readonly ButtonType Reset = new("reset");
+
     public static readonly ButtonType Link = new("link");
 }
